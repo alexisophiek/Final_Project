cbc0d903ba1d4c0e89607771df2f1b018ed41af1
Alexis <alexisophie.k@gmail.com>
2020-02-15 10:37:28 -0800

    config.py with bearer-token

---
+++ config.py
@@ -0,0 +1,12 @@
+API_Key = "t2MRkMfCDluKLl7P08vl9HaNF"
+API_SecretKey = "ZRki4rWCaQrfkpad6JP24bD8qELrOq0HdqTAQwBMpnZaZ3eXpE"
+
+Access_Token = "116007014-HAVZc6Z2L0aJTdfzYLhdugkO9wSiqm8sNEOYFEi8"
+Access_SecretToken = "o6jdZ1TnpWVjptGUeOHJ2n7nbHHPu3uGoXCHjxWAcHOTr"
+
+Bearer_Token = "AAAAAAAAAAAAAAAAAAAAAKCWCQEAAAAAfQ5tJUd0NhsRf2CI15wGmCeROpg%3DMLvJC1v3R5zfLSgEXHxgGOgygMrHQpyHYxssBUxIE1wbGPnaIj"
+
+Dev_EnvLabel = "ProjectProduction"
+
+# curl bearer token response:
+# {"token_type":"bearer","access_token":"AAAAAAAAAAAAAAAAAAAAAKCWCQEAAAAAfQ5tJUd0NhsRf2CI15wGmCeROpg%3DMLvJC1v3R5zfLSgEXHxgGOgygMrHQpyHYxssBUxIE1wbGPnaIj"}(pythondata)
