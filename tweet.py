c52202d192be4deef0dd8324326b174ad48edb55
Alexis <alexisophie.k@gmail.com>
2020-02-17 19:05:55 -0800

    tweet.py and possible Json?

--- tweet.py
+++ tweet.py
@@ -4,23 +4,29 @@ import tweepy
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
 
-# https://stream.twitter.com/1.1/statuses/filter.json?track=twitter
-
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print(tweet.text)
-
-#override tweepy.StreamListener to add logic to on_status
+# #override tweepy.StreamListener to add logic to on_status
 class MyStreamListener(tweepy.StreamListener):
-
     def on_status(self, status):
         print(status.text)
 
+
 myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
-
-myStream.filter(track=['election2020'])
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
 
 
+def main():
+    with open('output.json', 'a') as f:
+        for line in myStream.filter(track=['election2020'], languages='en'):
+            f.write(json.dumps(line))
+            f.write('\n')
+
+
+if __name__ == '__main__':
+    main()
