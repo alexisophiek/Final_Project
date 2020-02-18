a7baed60179632453e37e64b01193fa1e6f203eb
Alexis <alexisophie.k@gmail.com>
2020-02-17 18:39:54 -0800

    Streaming - how to store?

--- tweet.py
+++ tweet.py
@@ -6,10 +6,21 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+# https://stream.twitter.com/1.1/statuses/filter.json?track=twitter
 
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print(tweet.text)
 
+#override tweepy.StreamListener to add logic to on_status
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status.text)
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+
+myStream.filter(track=['election2020'])
 
 
