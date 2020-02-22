c4e8e1a839636886c640bd7cdb80903e087a131d
Sam Cooper <samcooperpdx@gmail.com>
2020-02-22 13:10:27 -0800

    sams notebook

--- config.py
+++ config.py
@@ -7,12 +7,14 @@ consumer_secret = ""
 access_token = ""
 access_token_secret = ""
 
-bearer_token = ""
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAISABgEAAAAAQSuCk498ieWouPFTHjcqpkM22xI%3DCb1YtB8Ta0mnk5TDtH4A4E2UdBtJpHppR768TIELDOy5dCRv5Z"
 
 dev_environment_label = ""
 
-pguser = ""
-pw = ""
+pguser = "sam"
+pw = "dataisok"
 
 # curl bearer token response:
-# {"token_type":"bearer","access_token":"AAAAAAAAAAAAAAAAAAAAAKCWCQEAAAAAfQ5tJUd0NhsRf2CI15wGmCeROpg%3DMLvJC1v3R5zfLSgEXHxgGOgygMrHQpyHYxssBUxIE1wbGPnaIj"}(pythondata)
+# {"token_type":"bearer","access_token":"AAAAAAAAAAAAAAAAAAAAAISABgEAAAAAQSuCk498ieWouPFTHjcqpkM22xI%3DCb1YtB8Ta0mnk5TDtH4A4E2UdBtJpHppR768TIELDOy5dCRv5Z"}(pythondata)
+
+# AAAAAAAAAAAAAAAAAAAAAISABgEAAAAAQSuCk498ieWouPFTHjcqpkM22xI%3DCb1YtB8Ta0mnk5TDtH4A4E2UdBtJpHppR768TIELDOy5dCRv5Z
