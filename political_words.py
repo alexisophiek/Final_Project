89676b6a78b6d2604920504af211c90430ba27e3
Erica Frisch <frisch.erica@gmail.com>
2020-02-27 22:47:08 -0800

    added political words list

---
+++ political_words.py
@@ -0,0 +1,20 @@
+words = ['healthcare', 'terrorism', 'gun','guns', 'education', 'economy', 'immigration', 'climate', \
+         'wealth', 'distribution', 'abortion', 'federal' 'budget', 'taxes', 'racism', 'sexism', 'metoo', \
+         'diplomacy', 'lgbqt','lgbtq', 'equality', 'trade', 'war', 'democrat', 'republican', 'socialist', "communist", \
+         'fascist', 'foreign', 'inclusivity', 'borders', 'medicare','nationalist','medicaid','democratic',\
+        'inequality','justice','legalize','illegal','drugs','prescription','women','womens','rights','universal',\
+        'supreme','court','potus','scotus','judge','maga','kaga','jobs','voting','voter','voters','daca',\
+        'citizen','election','incumbent','independent','nominee','pac','party','lobbyist','gerrymandering',\
+        'russia', 'russian', 'china', 'chinese', 'coronavirus', 'iowa', 'nevada', 'supertuesday', 'debate', \
+         'debates', 'delegates', 'delegate', 'cnn', 'fox', 'golf', 'impeach', 'impeachment', 'senate', 'congress',\
+         'capital','feelthebern', 'peteforamerica', 'ourbestdaysstilllieahead', 'notmeus', 'wintheera', \
+         'dreambigfighthard', 'ilikemike', 'math', 'promisesmadepromiseskept', 'jobsnotmobs', 'keepamericagreat',\
+        'believe', 'beliefs', 'bullshit', 'christian', 'family', 'values', 'millennials', 'latino', 'latina', 'latinx',\
+        'grassroots','balance','policy','poll','platform','muslim','isis','arab','arabic','ewarren','berniesanders',\
+        'petebuttigieg','realdonaltrump','mikebloomberg','joebiden','criminal', 'vice', 'epidemic', 'wrong', 'right',\
+         'left','greed', 'threat', 'affordable', 'reform', 'promise', 'progress', 'support', 'protect', \
+         'crisis', 'hate', 'debt', 'safe', 'freedom', 'win', 'lose','deficit','stock','market','nasdeq','hack','hacking',\
+        'sanctuary','dissent','divide','divided','ballot','bailout','primary','usa','united','states','america','fraud',\
+        'black','african','american','africanamerica','blacklivesmatter','blm','campgain','finance','civic','duty',\
+         'altright','nazi','nazis','neonazi','neonazis','wage','wages','income','base','gop','epstein','capitalist',\
+         'capitalism','dreamer','dreamers','border','detention','bluelivesmatter','ivanka','donald','melania']
