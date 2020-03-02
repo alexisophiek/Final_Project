81ec8d51b8eaf2505cc6d95b1c89802bcdc9d572
Erica Frisch <frisch.erica@gmail.com>
2020-03-02 11:23:46 -0800

    fix typo

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def get_cleaned():
 @app.route("/word_cloud")
 def get_words():
     cloud = pd.read_sql("select * from word_cloud",con=engine).rename(columns={"word":"x"})
-    return json.dumps(cloud)
+    return cloud.to_json()
 
 @app.route("/word_vis")
 def get_word_vis():
